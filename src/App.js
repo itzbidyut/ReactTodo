da5240179fd31436ee2d016da6d25b9a782ea1a0
itzbidyut <bbboruah1994@gmail.com>
2019-11-18 22:58:34 +0530

    uuid

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 
 import TodoList from './Component/TodoList'
 import Item from './Component/Item'
+import uuid from 'uuid';
 
 import '../src/App.css'
 
+
+
 class App extends Component {
+  state={
+   /*  items:[{id:1,
+    title:'wake up'},
+    {id:2,
+      title:'wake up',
+  ] */
+  }
   render() {
     return (
       <div className="App">
